a11d472b586fe3e1c929f0a51ac1cc7f36a54608
rocktronica <tommy@iamnotagoodartist.com>
2013-02-02 20:30:43 -0800

    Refactoring

--- dmv.py
+++ dmv.py
@@ -3,37 +3,38 @@ import sys
 from random import randint
 import argparse
 
-# fix: questions_asked/questions_to_ask, avoid duplicates
-# ideas: keep a list of problem questions, reprint failed questions!
+# ideas: answer as sentence, keep a list of problem questions, avoid duplicates, reprint failed questions!
 
 questions = json.load(open("questions.json"))
 
 def get_random_question():
     return questions[randint(0, len(questions)) - 1]
 
-def prompt_question(question):
-    print question.get("label")
-
-    correct_answer_letter = None
+def get_correct_letter(question):
     for i, option in enumerate(question.get("options")):
-        option_letter = chr(i + 65)
-        print "  " + option_letter + ": " + option.get("text")
         if option.get("isAnswer"):
-            correct_answer_letter = option_letter
+            return index_to_letter(i)
 
-    got_correct = True
+def print_question(question):
+    print question.get("label")
+    for i, option in enumerate(question.get("options")):
+        print "  " + index_to_letter(i) + ": " + option.get("text")
+
+def prompt_for_answer(question):
+    correct_letter = get_correct_letter(question)
 
     user_answer_letter = raw_input("  ?: ").upper()
-    while (not user_answer_letter == "Q" and not user_answer_letter == correct_answer_letter):
-        user_answer_letter = raw_input("  X: ").upper()
-        got_correct = False
 
-    # maybe this could return a (correct: false, continue: true, etc)
-    if user_answer_letter == "Q":
-        return None
-    else:
-        print "That's right: " + user_answer_letter + "! \n"
-        return got_correct
+    return {
+        'letter': user_answer_letter,
+        'correct': user_answer_letter == correct_letter
+    }
+
+def index_to_letter(index):
+    return chr(index + 65)
+
+def percent_score(correct, total):
+    return str(int(float(questions_correct) / float(questions_total) * 100)) + "%"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -44,16 +45,37 @@ if __name__ == '__main__':
     questions_to_ask = args.questions or 5
     questions_correct = 0
 
-    keep_going = True
+    continue_questions = True
 
     print
 
-    while (keep_going and questions_total < questions_to_ask):
-        got_answer_correct = prompt_question(get_random_question())
-        if got_answer_correct:
-            questions_correct += 1
-        keep_going = not got_answer_correct == None
-        if keep_going:
-            questions_total += 1
+    while (continue_questions and questions_total < questions_to_ask):
+        question = get_random_question()
+        asking = True
+        answered = True
+        correct = True
 
-    print "Your score: " + str(questions_correct) + "/" + str(questions_total) + " - " + str(int(float(questions_correct) / float(questions_total) * 100)) + "%"
+        print_question(question)
+
+        while (asking):
+            user_answer = prompt_for_answer(question)
+            if (user_answer.get("letter") == "Q"):
+                asking = False
+                answered = False
+                continue_questions = False
+            elif (user_answer.get("correct")):
+                asking = False
+            elif (not user_answer.get("correct")):
+                correct = False
+
+        print
+
+        if answered:
+            questions_total += 1
+            print "  That's right! \n"
+            if correct:
+                questions_correct += 1
+
+    if (questions_total > 0):
+        print "Correct: " + str(questions_correct) + "/" + str(questions_total)
+        print "Score:   " + percent_score(questions_correct, questions_total)
